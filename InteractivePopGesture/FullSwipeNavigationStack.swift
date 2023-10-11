126379d8708db249b56f706b1e1880d39222c640
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-10-11 15:09:34 -0500

    first commit

---
+++ InteractivePopGesture/FullSwipeNavigationStack.swift
@@ -0,0 +1,18 @@
+//
+//  FullSwipeNavigationStack.swift
+//  InteractivePopGesture
+//
+//  Created by Justin Hold on 10/11/23.
+//
+
+import SwiftUI
+
+struct FullSwipeNavigationStack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FullSwipeNavigationStack()
+}
