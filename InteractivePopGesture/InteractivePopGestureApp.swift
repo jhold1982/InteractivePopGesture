cf0573db7a44f26d7d9adb53e1694976acf8b266
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-10-11 13:02:34 -0500

    Initial Commit

---
+++ InteractivePopGesture/InteractivePopGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractivePopGestureApp.swift
+//  InteractivePopGesture
+//
+//  Created by Justin Hold on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct InteractivePopGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
